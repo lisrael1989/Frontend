bf2fc7ee945ee99970aa5c33cf0c8dad036c9221
yossefdahan <yossefdahan1@gmail.com>
2024-07-31 11:29:01 +0300

    Add basic routes

---
+++ src/cmps/Footer.jsx
@@ -0,0 +1,7 @@
+export function Footer() {
+    return (
+        <footer>
+            <p>Copyright © 2022. All rights reserved.</p>
+        </footer>
+    )
+}
---
+++ src/pages/EatIndex.jsx
@@ -0,0 +1,7 @@
+export function EatIndex() {
+    return (
+        <div>
+            <h1>EatIndex</h1>
+        </div>
+    )
+}
