17bbaa276309a86ff498d6f9cc5a21a322ef9885
israel litvak <lisrael1989@gmail.com>
2024-07-31 13:42:02 +0300

    add services files

---
+++ src/services/rest.service.js
@@ -0,0 +1,25 @@
+import { httpService } from './http.service.js';
+import { utilService } from './util.service.js';
+
+const BASE_URL = 'rest/';
+
+export const restService = {
+  query,
+  getById,
+  save,
+  remove,
+  getEmptyRest,
+  addrestMsg,
+  //   getLabels,
+  //   getFilterFromParams,
+};
+
+window.cs = restService;
+
+async function query(filterBy) {
+  return httpService.get(BASE_URL, { params: { filterBy } });
+}
+
+function getById(stayId) {
+  return httpService.get(`stay/${stayId}`);
+}
