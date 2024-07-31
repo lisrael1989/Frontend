c8593e345f62d32955dc3e4ddcf4fdeb78a5b903
israel litvak <lisrael1989@gmail.com>
2024-07-31 11:47:55 +0300

    Basic scss setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,21 @@
-
-import './App.css'
-import { EatIndex } from './pages/EatIndex'
-import { Header } from './cmps/Header'
-import { Footer } from './cmps/Footer'
-import { Route, Router, Routes } from 'react-router'
+import './App.css';
+import { EatIndex } from './pages/EatIndex';
+import { Header } from './cmps/Header';
+import { Footer } from './cmps/Footer';
+import { Route, Router, Routes } from 'react-router';
 
 export function App() {
-
-
   return (
-    <div>
-
+    <div className="main-container">
       <Header />
 
       <>
-
         <Routes>
           <Route path="/" element={<EatIndex />} />
         </Routes>
       </>
 
-
-
       <Footer />
-
     </div>
-  )
+  );
 }
-
-
