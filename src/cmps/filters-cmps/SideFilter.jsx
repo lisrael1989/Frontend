8af498188e6552fc0c893ce95000c6e834a8616d
yossefdahan <yossefdahan1@gmail.com>
2024-08-04 14:29:54 +0300

    Add sort cmp style

--- src/cmps/filters-cmps/SideFilter.jsx
+++ src/cmps/filters-cmps/SideFilter.jsx
@@ -9,7 +9,7 @@ export function SideFilter() {
     const filterBy = useSelector(storeState => storeState.restModule.filterBy)
     const sortBy = useSelector(storeState => storeState.restModule.sortBy)
     const [clearFilterBtn, setClearFilterBtn] = useState(false)
-
+    const [openSort, setOpenSort] = useState(false)
     useEffect(() => {
         setClearFilterBtn(false);
     }, [clearFilterBtn]);
@@ -31,6 +31,11 @@ export function SideFilter() {
         }
         return 'clear-filter-btn'
     }
+
+    function handleToggleOpenSort() {
+        setOpenSort(!openSort)
+    }
+
     return (
         <aside className="side-filter-container">
             <div className="filter-header">
@@ -50,7 +55,21 @@ export function SideFilter() {
                 onSetFilterBy={onSetFilterBy}
                 clearFilterBtn={clearFilterBtn}
             />
-            <SortFilter onSetSortBy={onSetSortBy} clearFilterBtn={clearFilterBtn} />
+            <div className="sort-container">
+                <div className="sort-header">
+                    <p>Sort list by</p>
+                    <button className="open-sort-btn" onClick={handleToggleOpenSort}>
+                        {openSort ? (
+                            <svg id="arrow-up" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"></polyline></svg>
+                        ) : (
+                            <svg id="arrow-down" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+                        )}
+                    </button>
+                </div>
+                <div className={openSort ? "open-sort-cmp on" : "open-sort-cmp"}>
+                    < SortFilter onSetSortBy={onSetSortBy} sortBy={sortBy} clearFilterBtn={clearFilterBtn} />
+                </div>
+            </div>
         </aside>
     );
 }
--- src/cmps/filters-cmps/SortFilter.jsx
+++ src/cmps/filters-cmps/SortFilter.jsx
@@ -1,23 +1,26 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
-export function SortFilter({ onSetSortBy, clearFilterBtn }) {
+export function SortFilter({ onSetSortBy, clearFilterBtn, sortBy }) {
+    const [selectedSort, setSelectedSort] = useState(sortBy.sortBy || "null")
     useEffect(() => {
         if (clearFilterBtn) {
             onSetSortBy({ sortBy: "" })
+            setSelectedSort("")
         }
     }, [clearFilterBtn])
     function setSortClick(sortBy) {
+        setSelectedSort(sortBy)
         onSetSortBy({ sortBy: sortBy })
     }
 
     return (
         <section className="sort-filter-container">
-            <button onClick={() => setSortClick("discounts")}>Discounts</button>
-            <button onClick={() => setSortClick("distance")}>Distance</button>
-            <button onClick={() => setSortClick("rating")}>Rating</button>
-            <button onClick={() => setSortClick("min")}>Min</button>
-            <button onClick={() => setSortClick("deliveryFee")}>Delivery fee</button>
-            <button onClick={() => setSortClick("deliveryTime")}>Delivery time</button>
+            <button className={selectedSort === "discounts" ? "active" : ""} onClick={() => setSortClick("discounts")}>Discounts</button>
+            <button className={selectedSort === "distance" ? "active" : ""} onClick={() => setSortClick("distance")}>Distance</button>
+            <button className={selectedSort === "rating" ? "active" : ""} onClick={() => setSortClick("rating")}>Rating</button>
+            <button className={selectedSort === "min" ? "active" : ""} onClick={() => setSortClick("min")}>Min</button>
+            <button className={selectedSort === "deliveryFee" ? "active" : ""} onClick={() => setSortClick("deliveryFee")}>Delivery fee</button>
+            <button className={selectedSort === "deliveryTime" ? "active" : ""} onClick={() => setSortClick("deliveryTime")}>Delivery time</button>
         </section>
     )
 }
