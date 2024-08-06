f93875a0127360fa0ab57d9d643b20be58c99c9c
yossefdahan <yossefdahan1@gmail.com>
2024-08-06 14:51:39 +0300

    Add style to main page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { EatIndex } from './pages/EatIndex';
 import { Header } from './cmps/Header';
 import { Footer } from './cmps/Footer';
 import { Route, Router, Routes } from 'react-router';
+import { RestDetails } from './pages/RestDetails';
 
 export function App() {
   return (
@@ -12,6 +13,7 @@ export function App() {
       <>
         <Routes>
           <Route path="/" element={<EatIndex />} />
+          {/* <Route path="/rest" element={<RestDetails />} /> */}
         </Routes>
       </>
 
---
+++ src/pages/RestDetails.jsx
@@ -0,0 +1,5 @@
+export function RestDetails() {
+    return (
+        <div>RestDetails</div>
+    )
+}
