e47ea294cfda5868625a1ab2fa3414af72065153
yossefdahan <yossefdahan1@gmail.com>
2024-08-06 09:40:01 +0300

    Fix bug with image

--- src/cmps/Header.jsx
+++ src/cmps/Header.jsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <>
       <header>
-        <img className="logo" src="public/img/EATNOW-NEW.png" alt="" />
+        <img className="logo" src="Img/EATNOW-NEW.png" alt="" />
         <h2 className="login">Login</h2>
       </header>
       <HeaderFilter />
